b9fe9c3f37c4ceb51d3c86875bcc47658ac289a0
Dowglas Maia <dowglasmaia@gmail.com>
2019-05-19 13:44:11 -0300

    Conf Recursos para Usar a API imemory no module do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+
+import { InMemoryDatabase } from './in-memory-database';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // Confg para usar a API REST InMemoryDatabase, necessario remover para usar API REST Externa.
   ],
   providers: [],
   bootstrap: [AppComponent]
