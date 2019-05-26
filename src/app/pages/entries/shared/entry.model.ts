5b2573b258ebca41dfaf0e32b10bca53dd768686
Dowglas Maia <dowglasmaia@gmail.com>
2019-05-26 20:05:20 -0300

    Merge

---
+++ src/app/pages/entries/shared/entry.model.ts
@@ -0,0 +1,29 @@
+import { Category } from './../../categories/shared/category.model';
+
+export class Entry {
+
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public type?: string,
+        public amount?: string,
+        public date?: string,
+        public paid?: boolean,
+        public categoryId?: number,
+        public category?: Category,
+
+    ) { }
+
+    //Tipo de Despesas. para setar os valores dinamicamente no Select do formulario
+    static types = {
+        expense: 'Despesa',
+        revenue: 'Receita',
+        ///
+    }
+
+     // Metodo, que passa os valores, pago ou pendente.
+    get paidText() : string {
+        return this.paid ? 'Pago' : 'Pendente';
+    }
+}
