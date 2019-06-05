87ed7de541d49e2e5dd72b3082a3f7b3715e503b
Dowglas Maia <dowglasmaia@gmail.com>
2019-06-04 22:32:08 -0300

    Addgin baese resource model

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,9 +1,13 @@
-export class Category {
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
+
+export class Category extends BaseResourceModel {
 
     constructor(
         public id?: number,
         public name?: string,
         public description?: string,
-    ) { }
+    ) {
+        super(); // sempre que se extent uma class teve declarar o super();
+    }
 
 }
--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,6 +1,7 @@
 import { Category } from './../../categories/shared/category.model';
+import { BaseResourceModel } from 'src/app/shared/models/base-resource.model';
 
-export class Entry {
+export class Entry extends BaseResourceModel {
 
     constructor(
         public id?: number,
@@ -13,7 +14,9 @@ export class Entry {
         public categoryId?: number,
         public category?: Category,
 
-    ) { }
+    ) {
+        super();
+    }
 
     //Tipo de Despesas. para setar os valores dinamicamente no Select do formulario
     static types = {
@@ -21,8 +24,8 @@ export class Entry {
         revenue: 'Receita',
     }
 
-     // Metodo, que passa os valores, pago ou pendente.
-    get paidText() : string {
+    // Metodo, que passa os valores, pago ou pendente.
+    get paidText(): string {
         return this.paid ? 'Pago' : 'Pendente';
     }
 }
