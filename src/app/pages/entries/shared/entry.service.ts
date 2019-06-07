7ac4b26292331197cecc901cd93ebca550202b2b
Dowglas Maia <dowglasmaia@gmail.com>
2019-06-06 23:37:34 -0300

    Atualizando  EntryServices, erdando da Class BaseResourceServices

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,15 +1,19 @@
 import { Injectable, Injector } from '@angular/core';
-import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators';
 
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class EntryService extends BaseResourceService<Entry>{ 
+export class EntryService extends BaseResourceService<Entry>{
+
 
   constructor(
     protected injector: Injector,
@@ -17,7 +21,5 @@ export class EntryService extends BaseResourceService<Entry>{
   ) {
     super("api/entries", injector);
   }
-
-
 
 }
