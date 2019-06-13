e4b1ca24ac75077ec4bf53af9e20a0371be3b2e9
Dowglas Maia <dowglasmaia@gmail.com>
2019-06-13 15:04:03 -0300

    Melhorando Formulario de Lançamentos

--- src/app/pages/entries/entry-form/entry-form.component.ts
+++ src/app/pages/entries/entry-form/entry-form.component.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Component, Injector, OnInit } from '@angular/core';
+import { Validators } from "@angular/forms";
 
 import { Entry } from "../shared/entry.model";
 import { EntryService } from "../shared/entry.service";
-
-import { switchMap } from "rxjs/operators";
-
-import toasrt from "toastr"; /* Para Exibir as Mensagens  */
-import { text } from '@angular/core/src/render3';
+import { BaseResourceFormComponent } from "../../../shared/base-resource-form/base-resource-form.component";
 import { Category } from './../../categories/shared/category.model';
 import { CategoryService } from '../../categories/shared/category.service';
 
@@ -18,14 +13,8 @@ import { CategoryService } from '../../categories/shared/category.service';
   templateUrl: './entry-form.component.html',
   styleUrls: ['./entry-form.component.css']
 })
-export class entryFormComponent implements OnInit, AfterContentChecked {
+export class entryFormComponent extends BaseResourceFormComponent<Entry> implements OnInit {
 
-  currentAction: string;
-  entryForm: FormGroup;
-  pageTitle: string;
-  serverErrorMessages: string[] = null; // Error do Servidor
-  submittingForm: boolean = false;
-  entry: Entry = new Entry();
   categories: Array<Category>;
 
   /* Configuração do Imask, Mascara da Moeda - Padrõ Brasil*/
@@ -53,133 +42,23 @@ export class entryFormComponent implements OnInit, AfterContentChecked {
   };
 
   constructor(
-    private entryService: EntryService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private categoriesService: CategoryService
-  ) { }
+    protected entryService: EntryService,
+    protected categoriesService: CategoryService,
+    protected injector: Injector
+  ) {
+    super(injector, new Entry(), entryService, Entry.fromJson)
+  }
 
   ngOnInit() {
-    this.setCurrentAction();
-    this.buildEntryForm();
-    this.loadentry();
-
     this.getGategories();
+    super.ngOnInit(); // para executar o ngOnInit da Classe Base tbm
   }
 
   /* Find Gategorias*/
-  private getGategories() {
+  protected getGategories() {
     this.categoriesService.getAll().subscribe(
       obj => this.categories = obj
     );
-  }
-
-  /* Carrega logo todos os componentes da pagina serem carregados*/
-  ngAfterContentChecked(): void {
-    this.setPageTitle();
-  }
-
-  submitForm() {
-    this.submittingForm = true;
-
-    if (this.currentAction == "new") {
-      this.createEntry();
-    } else {
-      this.update();
-    }
-  }
-
-  // PRIVATE METHODS
-
-  /* Verificando o tipo da operção, se é  new ou edit, pegando pela url ativa*/
-  private setCurrentAction() {
-    if (this.route.snapshot.url[0].path == "new")
-      this.currentAction = "new"
-    else
-      this.currentAction = "edit"
-  }
-
-  /* Criando o Formulario da Categoria*/
-  private buildEntryForm() {
-    this.entryForm = this.formBuilder.group({
-      id: [null],
-      name: ['', [Validators.required, Validators.minLength(2)]],
-      description: [''],
-      type: ["expense", [Validators.required]],
-      amount: [null, [Validators.required]],
-      date: [null, [Validators.required]],
-      paid: [true, [Validators.required]],
-      categoriId: [null],
-    })
-  }
-
-  /* Lenda as Cagetorias de acordo com a Operação Atual*/
-  private loadentry() {
-    if (this.currentAction == "edit") {
-      this.route.paramMap.pipe(
-        switchMap(params => this.entryService.getById(+params.get("id"))) // usar o sinal de +, para converter para um numero
-      )
-        .subscribe(
-          (entry) => {
-            this.entry = entry;
-            this.entryForm.patchValue(entry); // bind  - setando os valores do formulario
-          },
-          (error) => alert('Ocorreu um erro no Servidor, tente mas tarde.')
-        )
-    }
-  }
-
-  /* Definindo Titulo da Pagina de acordo com a Operação.*/
-  private setPageTitle() {
-    if (this.currentAction == "new") {
-      this.pageTitle = 'Cadastro de Novo Lançamento';
-    } else {
-      const entryName = this.entry.name || ''
-      this.pageTitle = 'Editando Lançamento: ' + entryName;
-    }
-  }
-
-  /* Update*/
-  private update() {
-    const entry: Entry = Entry.fromJson(this.entryForm.value);
-    this.entryService.update(entry).subscribe(
-      entry => this.actionsForSuccess(entry),
-      // toasrt.success("Lançamento atualizado com Sucesso!"),
-      error => this.actionsForError(error)
-    )
-
-  }
-
-  /* Salvar*/
-  private createEntry() {
-    const entry: Entry = Entry.fromJson(this.entryForm.value);
-
-    this.entryService.create(entry).subscribe(
-      entry => this.actionsForSuccess(entry),
-      error => this.actionsForError(error)
-    )
-  }
-
-  /* Quando operação é realizado com sucesso*/
-  private actionsForSuccess(obj: Entry): void {
-    toasrt.success("Solicitação Processada com Sucesso!");
-
-    this.router.navigateByUrl("entries", { skipLocationChange: true }).then(
-      () => this.router.navigate(["entries", obj.id, "edit"])
-    )
-  }
-
-  /* Error -  quando oceorre um error na Operação*/
-  private actionsForError(error) {
-    toasrt.error('Ocorreu um erro ao processar a sua solicitação!');
-    this.submittingForm = false;
-
-    if (error.status === 422) {
-      this.serverErrorMessages = JSON.parse(error._body).errors; //  retorna o Error do Servidor em Forma de Array
-    } else {
-      this.serverErrorMessages = [' Falha na comunicação com o Servidor. Por favor tente mas tarde.']
-    }
   }
 
   /* Definindo os Valores do Select , e Tipos*/
@@ -192,6 +71,30 @@ export class entryFormComponent implements OnInit, AfterContentChecked {
         }
       }
     )
+  }
+
+  /* Criando o Formulario da Categoria*/
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      description: [''],
+      type: ["expense", [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [true, [Validators.required]],
+      categoriId: [null],
+    })
+  }
+
+  /* Sobrescrevendo os metodos dos Titulos*/
+  protected creationPageTitle(): string {
+    return "Cadastro de Novo Lançamento";
+  }
+
+  protected editionPateTile(): string {
+    const entryName = this.resource.name || "";
+    return "Editando Lançamento: " + entryName;
   }
 
 }
