1465d50d87e7e180ec749d75853fe0c2a3de061d
Dowglas Maia <dowglasmaia@gmail.com>
2019-06-25 17:36:57 -0300

    Impl- Template

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
 
   /* url do servidor*/
-  url_api: 'http://localhost:8080',
+  url_api: 'http://maiati-env.aapjxeaf8q.us-east-2.elasticbeanstalk.com',
 
 
   /* headers - Definindo o Tipo de Conteudo que é Passo no corpo da requisição*/
